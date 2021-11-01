b31b49be76e9930ad0dfbe53e90addaed6beaaba
Zachary Sain <zachsain@gmail.com>
2021-11-01 11:23:08 -0500

    commit tree 57a599238c48d7ddc58435c6607e0865f3987ecb

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+
+  if (ride <= 400) {
+  return "This one is on me!";
+  }
+  if (ride <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } 
+  else if (ride > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+      case 'generous' : return 'Thank you so much.' 
+      case 'not as generous' : return 'Thank you.'
+      case 'thanks for everything' : return 'Bye.'
+      }
+
 }
